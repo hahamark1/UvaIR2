1eea1896cd31765a8f76819810f1f3d28184ff39
jorism1993 <jorismollinga@hotmail.com>
2018-11-20 12:24:43 +0100

    Kan gerund worden

--- constants.py
+++ constants.py
@@ -6,8 +6,8 @@ MAX_WORDS_GEN = 20
 # Cuda constants
 DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
-MAX_UTTERENCE_LENGTH = 150
-MAX_LENGTH = 150
+MAX_UTTERENCE_LENGTH = 30
+MAX_LENGTH = 30
 
 TEACHER_FORCING_RATIO = 0.5
 
