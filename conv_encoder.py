ce1e6c3fe37d12930ccee89aaa7a49c8046a75e6
jorism1993 <jorismollinga@hotmail.com>
2018-11-26 16:12:03 +0100

    update voor nieuwe code gevonden op git

--- conv_encoder.py
+++ conv_encoder.py
@@ -2,6 +2,8 @@ import torch
 from models.convolutional_encoder import FConvEncoder
 import torch.optim as optim
 import torch.nn as nn
+from models.model import AttnDecoderRNN
+from models.convolutional_generator import Generator
 from constants import *
 from dataloader.DailyDialogLoader import DailyDialogLoader, PadCollate
 from torch.utils.data import DataLoader
@@ -26,10 +28,22 @@ def load_dataset():
 
     return train_dd_loader, train_dataloader, test_dataloader
 
-def trainEpochs(encoder, train_dataloader, num_epochs=3000, print_every=100,
+
+def train(input_tensor, target_tensor, generator, optimizer):
+
+    optimizer.zero_grad()
+    loss = generator(input_tensor, target_tensor)
+    loss.backward()
+    optimizer.step()
+    target_length = target_tensor.shape[1]
+
+    return loss.item() / target_length
+
+
+def trainIters(generator, train_dataloader, test_dataloader, num_epochs=3000, print_every=100,
                evaluate_every=100, save_every=100, learning_rate=0.001):
 
-    optimizer = optim.RMSprop(encoder.parameters(), lr=learning_rate)
+    optimizer = optim.RMSprop(generator.parameters(), lr=learning_rate)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.9)
 
     num_iters = len(train_dataloader)
@@ -44,16 +58,146 @@ def trainEpochs(encoder, train_dataloader, num_epochs=3000, print_every=100,
 
         for i, (input_tensor, target_tensor) in enumerate(train_dataloader):
 
-            hidden, attention = encoder.forward(input_tensor)
+            input_tensor, target_tensor = input_tensor.to(DEVICE), target_tensor.to(DEVICE)
 
-            print(hidden.shape)
+            loss = train(input_tensor, target_tensor, generator, optimizer)
+
+            iter_loss += loss
+            epoch_loss += loss
+
+            if iter % print_every == 0 and iter > 0:
+                iter_loss_avg = iter_loss / print_every
+                print('Average loss of the last {} iters {}'.format(print_every, iter_loss_avg))
+
+                iter = 0
+                iter_loss = 0
+
+            if num_iters % evaluate_every == 0:
+                test_sentence = input_tensor[0, :]
+                test_target_sentence = target_tensor[0, :]
+
+                real_test_sentence = train_dataloader.dataset.vocabulary.tokens_to_sent(test_sentence)
+                real_target_sentence = train_dataloader.dataset.vocabulary.tokens_to_sent(test_target_sentence)
+
+                decoded_words = evaluate(generator.encoder, generator.decoder, test_sentence)
+                generated_sentence = train_dataloader.dataset.vocabulary.list_to_sent(decoded_words)
+
+                print(real_test_sentence)
+                print('>>')
+                print(generated_sentence)
+                print('==')
+                print(real_target_sentence)
+                print('-----------------------------')
+
+            if num_iters % save_every == 0 and num_iters > 0:
+                torch.save(generator, os.path.join('saved_models', 'generator_{}.pt'.format(MAX_UTTERENCE_LENGTH)))
+
+            num_iters += 1
+            iter += 1
+
+        scheduler.step()
+
+        epoch_loss_avg = epoch_loss / i
+        losses.append((epoch_loss_avg))
+        print('After epoch {} the loss is {}'.format(epoch, epoch_loss_avg))
+        average_score = evaluate_test_set(generator, test_dataloader)
+        blue_scores.append(average_score)
+        plot_blue_score(blue_scores)
+        plot_epoch_loss(losses)
+        print('After epoch {} the average Blue score of the test set is: {}'.format(epoch, average_score))
+
+
+def evaluate(encoder, decoder, input_tensor, max_length=MAX_LENGTH):
+    with torch.no_grad():
+
+        input_tensor = input_tensor.view(1, -1)
+        input_length = input_tensor.shape[1]
+
+        encoder_outputs = torch.zeros(max_length, 1, encoder.hidden_size, device=DEVICE)
+        encoder_hidden = None
+
+        for ei in range(input_length):
+            encoder_output, encoder_hidden = encoder(input_tensor[:, ei], encoder_hidden)
+            encoder_outputs[ei, :, :] = encoder_output[0, :, :]
+
+        decoder_input = torch.tensor([[SOS_INDEX]], device=DEVICE).view(-1, 1)  # SOS
+
+        decoder_hidden = encoder_hidden
+
+        decoded_words = []
+
+        for di in range(MAX_WORDS_GEN):
+            decoder_output, decoder_hidden, _ = decoder(decoder_input, decoder_hidden, encoder_outputs)
+            topv, topi = decoder_output.data.topk(1)
+
+            if topi.item() == EOS_INDEX:
+                decoded_words.append(EOS_INDEX)
+                break
+            else:
+                decoded_words.append(topi.item())
+
+            decoder_input = topi.detach()
+
+        return decoded_words
+
+def evaluate_test_set(generator, test_dataloader, max_length=MAX_LENGTH):
+
+    encoder = generator.encoder
+    decoder = generator.decoder
+
+    BLUE = BlueEvaluator(test_dataloader.dataset.vocabulary.index2word)
+
+    scores = []
+
+    with torch.no_grad():
+
+        for i, (input_tensor, target_tensor) in enumerate(test_dataloader):
+
+            input_tensor, target_tensor = input_tensor.to(DEVICE), target_tensor.to(DEVICE)
+
+            batch_size = input_tensor.shape[0]
+            input_length = input_tensor.shape[1]
+
+            encoder_outputs = torch.zeros(max_length, batch_size, encoder.hidden_size, device=DEVICE)
+            encoder_hidden = None
+
+            for ei in range(input_length):
+                encoder_output, encoder_hidden = encoder(input_tensor[:, ei], encoder_hidden)
+                encoder_outputs[ei, :, :] = encoder_output[0, :, :]
+
+            decoder_input = torch.tensor([[SOS_INDEX]], device=DEVICE).view(-1, 1)  # SOS
+
+            decoder_hidden = encoder_hidden
+
+            decoded_words = []
+
+            for di in range(MAX_WORDS_GEN):
+                decoder_output, decoder_hidden, _ = decoder(decoder_input, decoder_hidden, encoder_outputs)
+                topv, topi = decoder_output.data.topk(1)
+
+                if topi.item() == EOS_INDEX:
+                    decoded_words.append(EOS_INDEX)
+                    break
+                else:
+                    decoded_words.append(topi.item())
+
+                decoder_input = topi.detach()
+
+            score = BLUE.list_to_blue(decoded_words, target_tensor.cpu().tolist()[0])
+            scores.append(score)
+
+    average_score = sum(scores) / len(scores)
+    return average_score
 
 
 if __name__ == '__main__':
 
     dd_loader, train_dataloader, test_dataloader = load_dataset()
 
-    FConvEncoder = FConvEncoder(dd_loader.vocabulary.n_words)
+    embed_dim = 512
+    FConvEncoder = FConvEncoder(dd_loader.vocabulary.n_words, embed_dim=embed_dim)
+    AttnDecoderRNN = AttnDecoderRNN(hidden_size=embed_dim, output_size=dd_loader.vocabulary.n_words)
+    Generator = Generator(FConvEncoder, AttnDecoderRNN)
 
-    trainEpochs(FConvEncoder, train_dataloader)
+    trainIters(Generator, train_dataloader, test_dataloader, num_epochs=30)
 
---
+++ models/convolutional_decoder.py
@@ -0,0 +1,227 @@
+import torch
+import torch.nn as nn
+import math
+import torch.nn.functional as F
+
+
+class FConvDecoder(nn.Module):
+    """Convolutional decoder"""
+
+    def __init__(
+            self, vocab_size, embed_dim=512, out_embed_dim=256,
+            convolutions=((512, 3),) * 20, attention=True,
+            dropout=0.1, share_embed=False, left_pad=False):
+
+        super(FConvDecoder, self).__init__()
+
+        self.dropout = dropout
+        self.left_pad = left_pad
+        self.need_attn = True
+
+        convolutions = extend_conv_spec(convolutions)
+        in_channels = convolutions[0][0]
+
+        if isinstance(attention, bool):
+            # expand True into [True, True, ...] and do the same with False
+            attention = [attention] * len(convolutions)
+        if not isinstance(attention, list) or len(attention) != len(convolutions):
+            raise ValueError('Attention is expected to be a list of booleans of '
+                             'length equal to the number of layers.')
+
+        self.embed_tokens = nn.Embedding(vocab_size, embed_dim)
+
+        self.fc1 = nn.Linear(embed_dim, in_channels)
+        self.dropout1 = nn.Dropout(p=dropout)
+
+        self.projections = nn.ModuleList()
+        self.convolutions = nn.ModuleList()
+        self.attention = nn.ModuleList()
+        self.residuals = []
+
+        layer_in_channels = [in_channels]
+        for i, (out_channels, kernel_size, residual) in enumerate(convolutions):
+            if residual == 0:
+                residual_dim = out_channels
+            else:
+                residual_dim = layer_in_channels[-residual]
+            self.projections.append(nn.Linear(residual_dim, out_channels)
+                                    if residual_dim != out_channels else None)
+            self.convolutions.append(
+                LinearizedConv1d(in_channels, out_channels * 2, kernel_size,
+                                 padding=(kernel_size - 1), dropout=dropout)
+            )
+            self.attention.append(AttentionLayer(out_channels, embed_dim)
+                                  if attention[i] else None)
+            self.residuals.append(residual)
+            in_channels = out_channels
+            layer_in_channels.append(out_channels)
+
+        self.adaptive_softmax = None
+        self.fc2 = self.fc3 = None
+
+        if adaptive_softmax_cutoff is not None:
+            assert not share_embed
+            self.adaptive_softmax = AdaptiveSoftmax(num_embeddings, in_channels, adaptive_softmax_cutoff,
+                                                    dropout=adaptive_softmax_dropout)
+        else:
+            self.fc2 = nn.Linear(in_channels, out_embed_dim)
+            if share_embed:
+                assert out_embed_dim == embed_dim, \
+                    "Shared embed weights implies same dimensions " \
+                    " out_embed_dim={} vs embed_dim={}".format(out_embed_dim, embed_dim)
+                self.fc3 = nn.Linear(out_embed_dim, vocab_size)
+                self.fc3.weight = self.embed_tokens.weight
+            else:
+                self.fc3 = nn.Linear(out_embed_dim, vocab_size)
+                self.dropout3 = nn.Dropout(p=self.dropout)
+
+    def forward(self, prev_output_tokens, encoder_out_dict=None, incremental_state=None):
+        if encoder_out_dict is not None:
+            encoder_out = encoder_out_dict['encoder_out']
+            encoder_padding_mask = encoder_out_dict['encoder_padding_mask']
+
+            # split and transpose encoder outputs
+            encoder_a, encoder_b = self._split_encoder_out(encoder_out, incremental_state)
+
+        if self.embed_positions is not None:
+            pos_embed = self.embed_positions(prev_output_tokens, incremental_state)
+        else:
+            pos_embed = 0
+
+        if incremental_state is not None:
+            prev_output_tokens = prev_output_tokens[:, -1:]
+        x = self._embed_tokens(prev_output_tokens, incremental_state)
+
+        # embed tokens and combine with positional embeddings
+        x += pos_embed
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        target_embedding = x
+
+        # project to size of convolution
+        x = self.fc1(x)
+
+        # B x T x C -> T x B x C
+        x = self._transpose_if_training(x, incremental_state)
+
+        # temporal convolutions
+        avg_attn_scores = None
+        num_attn_layers = len(self.attention)
+        residuals = [x]
+        for proj, conv, attention, res_layer in zip(self.projections, self.convolutions, self.attention,
+                                                    self.residuals):
+            if res_layer > 0:
+                residual = residuals[-res_layer]
+                residual = residual if proj is None else proj(residual)
+            else:
+                residual = None
+
+            x = F.dropout(x, p=self.dropout, training=self.training)
+            x = conv(x, incremental_state)
+            x = F.glu(x, dim=2)
+
+            # attention
+            if attention is not None:
+                x = self._transpose_if_training(x, incremental_state)
+
+                x, attn_scores = attention(x, target_embedding, (encoder_a, encoder_b), encoder_padding_mask)
+
+                if not self.training and self.need_attn:
+                    attn_scores = attn_scores / num_attn_layers
+                    if avg_attn_scores is None:
+                        avg_attn_scores = attn_scores
+                    else:
+                        avg_attn_scores.add_(attn_scores)
+
+                x = self._transpose_if_training(x, incremental_state)
+
+            # residual
+            if residual is not None:
+                x = (x + residual) * math.sqrt(0.5)
+            residuals.append(x)
+
+        # T x B x C -> B x T x C
+        x = self._transpose_if_training(x, incremental_state)
+
+        # project back to size of vocabulary if not using adaptive softmax
+        if self.fc2 is not None and self.fc3 is not None:
+            x = self.fc2(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+            x = self.fc3(x)
+
+        return x, avg_attn_scores
+
+    def reorder_incremental_state(self, incremental_state, new_order):
+        super().reorder_incremental_state(incremental_state, new_order)
+        encoder_out = utils.get_incremental_state(self, incremental_state, 'encoder_out')
+        if encoder_out is not None:
+            encoder_out = tuple(eo.index_select(0, new_order) for eo in encoder_out)
+            utils.set_incremental_state(self, incremental_state, 'encoder_out', encoder_out)
+
+    def max_positions(self):
+        """Maximum output length supported by the decoder."""
+        return self.embed_positions.max_positions() if self.embed_positions is not None else float('inf')
+
+    def upgrade_state_dict(self, state_dict):
+        if utils.item(state_dict.get('decoder.version', torch.Tensor([1]))[0]) < 2:
+            # old models use incorrect weight norm dimension
+            for i, conv in enumerate(self.convolutions):
+                # reconfigure weight norm
+                nn.utils.remove_weight_norm(conv)
+                self.convolutions[i] = nn.utils.weight_norm(conv, dim=0)
+            state_dict['decoder.version'] = torch.Tensor([1])
+        return state_dict
+
+    def make_generation_fast_(self, need_attn=False, **kwargs):
+        self.need_attn = need_attn
+
+    def _embed_tokens(self, tokens, incremental_state):
+        if incremental_state is not None:
+            # keep only the last token for incremental forward pass
+            tokens = tokens[:, -1:]
+        return self.embed_tokens(tokens)
+
+    def _split_encoder_out(self, encoder_out, incremental_state):
+        """Split and transpose encoder outputs.
+        This is cached when doing incremental inference.
+        """
+        cached_result = utils.get_incremental_state(self, incremental_state, 'encoder_out')
+        if cached_result is not None:
+            return cached_result
+
+        # transpose only once to speed up attention layers
+        encoder_a, encoder_b = encoder_out
+        encoder_a = encoder_a.transpose(1, 2).contiguous()
+        result = (encoder_a, encoder_b)
+
+        if incremental_state is not None:
+            utils.set_incremental_state(self, incremental_state, 'encoder_out', result)
+        return result
+
+    def _transpose_if_training(self, x, incremental_state):
+        if incremental_state is None:
+            x = x.transpose(0, 1)
+        return x
+
+def LinearizedConv1d(in_channels, out_channels, kernel_size, dropout=0, **kwargs):
+    """Weight-normalized Conv1d layer optimized for decoding"""
+    m = LinearizedConvolution(in_channels, out_channels, kernel_size, **kwargs)
+    std = math.sqrt((4 * (1.0 - dropout)) / (m.kernel_size[0] * in_channels))
+    nn.init.normal_(m.weight, mean=0, std=std)
+    nn.init.constant_(m.bias, 0)
+    return nn.utils.weight_norm(m, dim=2)
+
+def extend_conv_spec(convolutions):
+    """
+    Extends convolutional spec that is a list of tuples of 2 or 3 parameters
+    (kernel size, dim size and optionally how many layers behind to look for residual)
+    to default the residual propagation param if it is not specified
+    """
+    extended = []
+    for spec in convolutions:
+        if len(spec) == 3:
+            extended.append(spec)
+        elif len(spec) == 2:
+            extended.append(spec + (1,))
+        else:
+            raise Exception('invalid number of parameters in convolution spec ' + str(spec) + '. expected 2 or 3')
+    return tuple(extended)
